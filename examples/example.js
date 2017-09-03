5bb38e29bae2c687fffd128eb9125372bdf221a2
Ricardo Sohn <ricardosohn@gmail.com>
2017-09-03 12:07:34 -0300

    Added the node-steam-tradeoffer-manager Events
    Added the following events to the base class:
    _onWebSession(sessionID, cookies) //Uses the previously generated cookies from the SteamUser to authenticate the community & manager modules
    _onNewOffer(offer) //Handles new incoming offers
    _onReceivedOfferChanged //Fired when the state of an existing offer is changed
    _onPollData(pollData) // Fired when polling data (might be from market, for example)

--- examples/example.js
+++ examples/example.js
@@ -2,11 +2,12 @@
 
 const SteamBot = require('../index.js');
 
-let bot = new SteamBot('myaccount', 'mypassword', {
-	sharedSecret: 'mysharedsecret',
-	groupID: 'steamgroupid',
-	gamePlayedName: 'This is what the bot will play as non steam game',
-	gamePlayed: 730 // this is the appid of the game run
+let bot = new SteamBot('account', 'password', {
+	sharedSecret: 'sharedSecret', //For this bot to work, you NEED to provide the shared secret
+	identitySecret: 'identitySecret', //For this bot to work, you NEED to provide the identity secret
+	groupID: '103582791460007047', // ID of the gruop that the bot will invite the users upon friendInvite accept
+	customGameName: 'Steam Bot', // Shows this custom name for the game playing
+	gamePlayed: 730 // AppID of the played game
 });
 
 bot.logOn(); // LogOn onto Steam
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const SteamUser = require('steam-user');
 const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const SteamTradeOfferManager = require('steam-tradeoffer-manager');
 const Winston = require('winston');
 
 const fs = require('fs');
@@ -14,17 +16,26 @@ class SteamBot {
 		this.password = password;
 	    this.options = options || {};
 
-	    this.groupID = this.options.groupID || undefined;
+	    this.service = this.options.service || undefined;
+	    this.apikey = this.options.apikey || undefined;
 	    this.logfile = this.options.logfile || this.username + '.log';
+	    this.guardCode = this.options.guardCode || undefined;
 	    this.twoFactorCode = this.options.twoFactorCode || undefined;
 	    this.sharedSecret = this.options.sharedSecret || undefined;
 	    this.identitySecret = this.options.identitySecret || undefined;
-	    this.confirmationInterval = this.options.confirmationInterval || undefined;
-	    this.gamePlayedName = this.options.gamePlayedName || undefined
+	    this.confirmationInterval = this.options.confirmationInterval || 30000;
 	    this.gamePlayed = this.options.gamePlayed || undefined;
-	    
-		this.client = new SteamUser();
 
+	    this.groupID = this.options.groupID || undefined;
+	    this.customGameName = this.options.customGameName || undefined
+
+	    this.client = new SteamUser();
+	    this.community = new SteamCommunity();
+	    this.manager = new SteamTradeOfferManager({
+	    	"steam": this.client,
+	    	"language": "en"
+	    });
+	    
 		this.logger = new (Winston.Logger)({
 	        transports: [
 	            new (Winston.transports.Console)({
@@ -42,12 +53,19 @@ class SteamBot {
 	            })
 	        ]
 	    });
-		//Steam Events
+
+		//SteamUser Events
 		this.client.on("loggedOn", (response) => { this._onLoggedOn(response) });
+		this.client.on("webSession", (sessionID, cookies) => { this._onWebSession(sessionID, cookies) } );
 		this.client.on("friendMessage", (steamID, message) => { this._onFriendMessage(steamID, message) });
 		this.client.on("friendRelationship", (steamID, relationship) => { this._onFriendRelationship(steamID, relationship) });
-	}
 
+		//TradeOfferManager Events
+		this.manager.on('newOffer', (offer) => { this._onNewOffer(offer); });
+		this.manager.on('receivedOfferChanged', (offer, oldState) => { this._onReceivedOfferChanged(offer, oldState); });
+		this.manager.on('pollData', (pollData) => { this._onPollData(pollData); });
+	}
+	
 	logOn(){
 		this.logger.info('Logging in...');
 		try{	
@@ -61,22 +79,36 @@ class SteamBot {
 		}
 	}
 
+	//Handlers (may be overriden)
 	_onLoggedOn(response){
 		if (response.eresult === SteamUser.EResult.OK) {
 			this.logger.info('Logged into Steam!');
 			this.client.setPersona(SteamUser.EPersonaState = 1);
-			if (this.gamePlayedName && this.gamePlayed){
-				this.client.gamesPlayed([this.gamePlayedName, this.gamePlayed]);
+			if(this.gamePlayed){
+				this.client.gamesPlayed([(this.customGameName ? this.customGameName : "SteamRDSBot"), this.gamePlayed]);
 			}
 		} else {
 		    this.logger.warn('EResult for logon: ' + response.eresult);
 		}
 	}
 
+	_onWebSession(sessionID, cookies){
+		this.manager.setCookies(cookies, (error) => {
+			if (error) {
+				this.logger.error("WebSession error :" + error);
+				process.exit(1); // Fatal error since we couldn't get our API key
+				return;
+			}
+			this.logger.info("Successfully received Steam API key");
+		});
+
+		this.community.setCookies(cookies);
+		this.community.startConfirmationChecker(this.confirmationInterval, this.identitySecret); // Checks and accepts confirmations every 30 seconds
+	}
+
 	_onFriendMessage(steamID, message){
 	    this.logger.info('Message from ' + steamID + ': ' + message);
-	    this.client.chatMessage(steamID, 'Hi, thanks for messaging me! If you are getting this message, it means that my ' +
-	                                     'owner hasn\'t configured me properly. Annoy them with messages until they do!');
+	    this.client.chatMessage(steamID, 'Hi, thanks for messaging me! My owner haven\'t finished me yet! You\'ll have to wait a little bite more for my services');
 	}
 
 	_onFriendRelationship(steamID, relationship){
@@ -86,9 +118,20 @@ class SteamBot {
 	        if (this.groupID) {
 	            this.client.inviteToGroup(steamID, this.groupID);
 	        }
-	        this.client.chatMessage(steamID, 'Hi, thanks for adding me! If you are getting this message, it means that my ' +
-	                                                'owner hasn\'t configured me properly. Annoy them with messages until they do!');
+	        this.client.chatMessage(steamID, 'Hi, thanks for adding me! My owner haven\'t finished me yet! You\'ll have to wait a little bite more for my services');
 	    }
+	}
+
+	_onNewOffer(offer){
+		this.logger.warn("TradeOfferManager: New offer #" + offer.id + " from " + offer.partner.getSteam3RenderedID() + ", but I won't do anything, because I am not configured yet!");
+	}
+
+	_onReceivedOfferChanged(offer, oldState){
+		this.logger.warn("TradeOfferManager: Offer #" + offer.id + " changed: " + TradeOfferManager.ETradeOfferState[oldState] + " => " + TradeOfferManager.ETradeOfferState[offer.oldState]+ ", but I won't do anything, because I am not configured yet!");
+	}
+
+	_onPollData(pollData){
+		this.logger.warn("TradeOfferManager: onPollData event fired, but I won't do anything, because I am not configured yet!");
 	}
 }
 
